9eca17f5de6e610af4b910cde6f363929f0491d7
Rick Liu <51324731+rick-lichen@users.noreply.github.com>
2022-07-10 22:43:31 -0500

    Update rsibbstop.py

--- rsibbstop.py
+++ rsibbstop.py
@@ -41,8 +41,8 @@ bought_price = None
 TRADE_QUANTITY = 8
 
 
-API_KEY = "ARs15CFBgEaUaHYCbtlkHooZ7zDAR7uCatdznUmvlfp0LADVPa0tjXVoVDol6MIg"
-API_SECRET = "K9yioiGEimDd36RMQn4Qizt3OzDuzMOAkQhXKF38mX46wP63YAYghsufa3X8zodV"
+API_KEY = "EXPIRED"
+API_SECRET = "EXPIRED"
 
 closes = []
 in_position = False
